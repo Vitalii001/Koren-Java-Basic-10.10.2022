09f6f99d03a51b04298bba6e674ee39d5d43f03e
[Vitalii Koren] <[korenvitalii1@gmail.com]>
2022-10-10 16:41:23 +0300

    Test commit1

---
+++ src/MyApp.java
@@ -0,0 +1,11 @@
+public class MyApp {
+    public static void main(String[] args) {
+        int a =5;
+        int b =4;
+        int c =3;
+        int volume =a*b*c;
+        System.out.println("Обьем паралелепипеда=" + volume);
+        int Lengt =a*4+b*4+c*4;
+        System.out.println("Сумарная длина всех сторон=" + Lengt);
+    }
+}
