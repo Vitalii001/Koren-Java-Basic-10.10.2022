630e726e1f2a509ec956ddf24a0f855f124509e2
[Vitalii Koren] <[korenvitalii1@gmail.com]>
2022-10-25 22:44:59 +0300

    DZ 7 v.2

--- DZ7shuttles/src/DZ7shuttles.java
+++ DZ7shuttles/src/DZ7shuttles.java
@@ -1,20 +1,23 @@
 public class DZ7shuttles {
     public static void main(String[] args) {
+        int numbers = 100; // Количество шатлов которое нужно прономеровать
+        System.out.println("Шатлы можно номеровать так:");
         int i =0;
-        int[] array = new int[100];
-        for (int i = 0; i < array.length; i++)
-        for(int a = 0; a < 10; a++){
-            for(int b = 0; b < 10; b++){
-                for(int c =0; c <10; c++){
-                    if( a != 4 && a != 9 &&  b != 4 && b != 9 && c !=4 && c !=9){
-                        int k = a*100 + b*10+ c;
-                        array[i] = k;
+        for (int a = 0; a < 10; a++) {
+            for (int b = 0; b < 10; b++) {
+                for (int c = 0; c < 10; c++) {
+                    if (a != 4 && a != 9 && b != 4 && b != 9 && c != 4 && c != 9) {
+                        int k = a * 100 + b * 10 + c;
+                        if (i<numbers && k !=0){
+                            System.out.println(k);
+                            i = i+1;
                         }
+                    }
+
                 }
-
             }
-        }
 
+        }
     }
 }
 
