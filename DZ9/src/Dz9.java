b0f1b8710d0d40fad00f125f1e4ca6af13b89180
[Vitalii Koren] <[korenvitalii1@gmail.com]>
2022-10-27 22:43:49 +0300

    Dz 9 v2.0

--- DZ9/src/Dz9.java
+++ DZ9/src/Dz9.java
@@ -5,34 +5,28 @@ public class Dz9 {
     public static void main(String[] args) {
         Random rand = new Random();
         int i1 = rand.nextInt(11);
+        int attempts = 5; //количество попыток
+        int attemptCounter = 1;
         System.out.println("Давай сыграем в игру 'Угадай число' ");
-        Scanner in = new Scanner(System.in);
-        System.out.println("Попытка №1");
-        System.out.print("Введите число от 0 до 10 : ");
-        int num = in.nextInt();
-        if (i1 == num) {
-            System.out.println("Вы угадали!");
-        } else {
-            System.out.println("Вы не угадали , попробуте снова!");
-            System.out.println("_______________________________");
-            Scanner in1 = new Scanner(System.in);
-            System.out.println("Попытка №2");
-            System.out.print("Введите число 0 до 10 : ");
-            int num1 = in1.nextInt();
-            if (num1 == i1) {
+
+        for (int i = 0; i < attempts; i++) {
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Попытка номер: " + attemptCounter);
+            System.out.print("Введите число от 0 до 10 : ");
+            attemptCounter++;
+            int num = in.nextInt();
+            if (i1 == num) {
                 System.out.println("Вы угадали!");
+                return;
             } else {
-                System.out.println("Вы не угадали , попробуте снова!");
-                System.out.println("_______________________________");
-                Scanner in2 = new Scanner(System.in);
-                System.out.println("Попытка №3");
-                System.out.print("Введите число 0 до 10 : ");
-                int num2 = in2.nextInt();
-                if (num2 == i1) {
-                    System.out.println("Вы угадали!");
-                } else {
+                if (attemptCounter == attempts + 1) {
                     System.out.println("Вы проиграли!");
+                } else {
+                    System.out.println("Вы не угадали , попробуте снова!");
+                    System.out.println("_______________________________");
                 }
+
             }
         }
     }
